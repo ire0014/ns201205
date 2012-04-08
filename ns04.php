5c3c2503f2029a52cea32cf955da1d8e22ac06b8
ire0014 <ire0014@gmail.com>
2012-04-08 23:55:49 +0900

    hello.php

---
+++ ns04.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+</head>
+<body>
+	<div>
+		HTMLテキスト
+<?php
+	echo "これはPHPのecho<br />";
+	$prt_text = "これはPHPのprint";
+	print($prt_text);
+?>
+	</div>
+</body>
+</html>
